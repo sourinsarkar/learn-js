c89da84086b5c8d5a5e09affe1780e7e607ed100
Sourin <iamsourinsarkar@gmail.com>
2023-08-21 10:10:39 +0530

    arrow and regular functions

--- starter.js
+++ starter.js
@@ -1,1 +1,59 @@
+// "use strict";
+// Variables
+
+// console.log(me);
+// console.log(job);
+// console.log(dob);
+
+// var me = 'Sourin';
+// let job = 'Student';
+// const dob = 2002;
+
+// -------------------------------------------------------------------------------------------------------
+
+// Functions
+
+// console.log(funDec(2, 3));
+// console.log(funEx(2, 4));
+// console.log(funArrow);
+// console.log(funArrow(2, 5));
+
+// function funDec(a, b) {
+//     return a + b;
+// }
+
+// const funEx = function (a, b) {
+//     return a + b;
+// }
+
+// const funArrow = (a, b) => a + b;
+
+// -------------------------------------------------------------------------------------------------------
+
+// this: keyword
+
+// console.log(this);
+
+// const age = function (birthYear) {
+    // return (2023 - brithYear);
+//     console.log(2023 - birthYear);
+//     console.log(this);
+// };
+
+// console.log(age(2002));
+
+// ----------------------------------------
+
+// this: keyword in arrow function
+
+// const age = (birthYear) => {
+//     console.log(2023 - birthYear);
+    // return (2023 - birthYear);
+//     console.log(this);
+// };
+
+// console.log(age(2002));
+
+// ----------------------------------------
+
 
