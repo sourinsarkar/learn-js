34d11832d65bf42efecc91b2777b81fa19255511
Sourin <iamsourinsarkar@gmail.com>
2023-09-03 23:55:03 +0530

    Solved Leetcode's 2634

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,14 +1,14 @@
-word) {
-    const words = word.split('_');
-    const newWord = [];
+{
+const words = word.split('_');
+const newWord = [];
 
-    for(const w of words) {
-        const [fname, lname] = w.toLowerCase().trim().split('_');
-        const newLname = lname.replace[lname[0], lname[0].toUpperCase()];
-        newWord.push(lname);
-        newWord.push(fname); 
-    }
-    console.log(newWord.join(''));
+for (const w of words) {
+    const [fname, lname] = w.toLowerCase().trim().split('_');
+    const newLname = lname.replace[lname[0], lname[0].toUpperCase()];
+    newWord.push(lname);
+    newWord.push(fname);
+}
+console.log(newWord.join(''));
 }
 
 capFL('sourin_sarkar');
