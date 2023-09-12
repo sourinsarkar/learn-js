fe0cd65bdb03a33578c50e2ae8c68f41f953a232
Sourin <iamsourinsarkar@gmail.com>
2023-09-12 16:25:53 +0530

    Completed Challenge 3

--- starter.js
+++ starter.js
@@ -1133,3 +1133,47 @@ const question = new Map([
 // console.log(question);
 // console.log([...question]);
 
+// Challenge 3 ----------------------------------------------
+
+// const gameEvents = new Map([
+//     [17, '⚽ GOAL'],
+//     [36, '� Substitution'],
+//     [47, '⚽ GOAL'],
+//     [61, '� Substitution'],
+//     [64, '� Yellow card'],
+//     [69, '� Red card'],
+//     [70, '� Substitution'],
+//     [72, '� Substitution'],
+//     [76, '⚽ GOAL'],
+//     [80, '⚽ GOAL'],
+//     [92, '� Yellow card'],
+// ]);
+
+// // Sol-1
+// const events = new Set(gameEvents.values());
+// console.log(events);
+
+// // Sol-2
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// // Sol-3
+// const theEvents = [...gameEvents];
+// const numberOfEvents = 90 / theEvents.length;
+// // console.log(`An event happened, on an average, every ${numberOfEvents} minutes.`);
+
+// // Alternative way
+// console.log(`An event happened, on an average, every ${90 / gameEvents.size} minutes.`);
+// const time = [...gameEvents.keys()];
+// console.log(time);
+
+// const lastElement = time.pop();
+// console.log(lastElement);
+// console.log(`An event happened, on an average, every ${lastElement / gameEvents.size} minutes.`);
+
+// // Sol-4
+// for(const [key, value] of gameEvents) {
+//     const half = key <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
+//     console.log(`${half} ${key} : ${value}`);
+// }
+
